7d93857d2087701ab341998a5382cb8e699fd0ee
Tod Loofbourrow <todloofbourrow@Tod-Loofbourrows-Macbook-Pro-2.local>
2013-10-05 16:42:47 -0400

    Setting Up the Database

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20131005203224) do
+
+  create_table "pics", :force => true do |t|
+    t.integer  "hour"
+    t.string   "emotion"
+    t.text     "learned"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
